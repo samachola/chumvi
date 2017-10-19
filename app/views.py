f8660fabe3a4d9fffe13e94ea04f5058e4341e15
sanachola <sam.achola@live.com>
2017-10-19 15:20:21 +0300

    UPDATE: Flask login and registration logic

--- app/views.py
+++ app/views.py
@@ -3,17 +3,30 @@ from app import app
 
 @app.route('/')
 def index():
+    session['show'] = True
     return render_template("index.html")
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    session['show'] = False
     if request.method == 'POST':
         if request.form['username'] :
             session['logged_in'] = True
             flash('Logged in successfully')
             return redirect(url_for('index'))
+    else:
+        session['logged_in'] = True
+        
     return render_template("login.html")
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    session['show'] = False
+    error = None
+    if request.method == 'POST':        
+        if not request.form['fname'] or not request.form['lname'] or not request.form['email'] or not request.form['password']:
+            error = "All input fields are required"
+        else:
+            return redirect(url_for('login'))
+
+    return render_template("register.html", error = error)
